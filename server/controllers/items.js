724dd22cf6626e81c29715cc469ae3aee0c9925a
Junaid Paracha <Junaid.p.1983@gamil.com>
2017-01-25 13:12:29 -0600

    Server Side Stuff for Walmart/Sams API

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose'),
-	request = require('request'),
-	moment = require('moment');
+		request = require('request'),
+		moment = require('moment');
+		walmart = require('walmart')('97x9uwsxg2u3uw5fbq77jg5f');
+		sams = require('sams-scraper')(require('isomorphic-fetch'));
 
 var Item = mongoose.model('Item'),
 	User = mongoose.model('User');
 
 function ItemsController() {
-
-	var week = 	moment().week().toString() + moment().year().toString();
 
 	var processError = function(error) {
 		var errors = [];
@@ -19,8 +19,15 @@ function ItemsController() {
 		return errors;
 	};
 
+	var lastSunday = function(){
+		var s = moment().format('YYYYMMDD');
+		var d = new Date(s.substring(0,4), s.substring(4,6) - 1, s.substring(6));
+	  d.setDate(d.getDate() - d.getDay());
+	  return d.toString().split(' ').join('');
+	}
 
 	this.index = function(req, res){
+		var week = lastSunday();
 
 		Item.find({active: true})
 			.populate({path:'comments',
@@ -43,8 +50,7 @@ function ItemsController() {
 			id: req.body.id,
 			from: req.body.from,
 			price: req.body.price,
-			category: req.body.category,
-			quantity: req.body.quantity
+			category: req.body.category
 		});
 
 		item.save(function(error, item) {
@@ -53,8 +59,6 @@ function ItemsController() {
 				res.json({ errors: processError(error) });
 				return;
 			}
-
-			console.log('item successfully created ->', item);
 
 			if(req.body.vote){
 				res.redirect('/items/'+ item._id +'/1');
@@ -73,52 +77,91 @@ function ItemsController() {
 				return;
 			}else{
 				req.params.vote = parseInt(req.params.vote);
+				console.log('type of vote-> ', typeof req.params.vote);
+				console.log('vote-> ', req.params.vote);
 
 				User.findOne({_id: req.session.user._id}, function(err,user){
+					if(!(user.votes.hasOwnProperty(itemId))){
+						user.votes[itemId] = req.params.vote;
+						user.markModified('votes');
+						user.save(function (err){
 
-					if( !(user.votes.hasOwnProperty(week)) || !(user.votes[week].hasOwnProperty(item._id))){
-						console.log('User has never voted on this before... vote is...', req.params.vote);
-						user.votes[week] = {};
-						user.votes[week][itemId] = req.params.vote;
-					}else{
-						user.votes[week][item._id] = -user.votes[week][item._id];
-					}
+							var week = lastSunday();
 
-					user.markModified('votes');
-					user.save(function (err){
+							console.log('vote: ==>', req.params.vote);
 
-						console.log('vote: ==>', req.params.vote);
+							if(!(item.voting_list.hasOwnProperty(week))){
+								console.log('this item has not been voted on before');
+								console.log('week info=> ',week);
+								console.log('typeof=> ', typeof week);
 
-						if(!(item.voting_list.hasOwnProperty(week))){
-							console.log('this item has not been voted on for this week before');
-							item.voting_list[week] = 0;
-							console.log('checking item voting list', item.voting_list);
-						}
+								item.voting_list[week] = 0;
 
-						if(req.params.vote>0){
-							item.voting_list[week]++;
-							console.log('checking item voting list (vote: 1)', item.voting_list);
-						}else{
-							if(item.voting_list[week]<2){
-								delete item.voting_list[week];
+								console.log('checking item voting list', item.voting_list);
+							}
+
+							if(req.params.vote>0){
+								item.voting_list[week]++;
+								console.log('checking item voting list (vote: 1)', item.voting_list);
 							}else{
-								item.voting_list[week]--;
-							}
-							console.log('checking item voting list (vote: 0)', item.voting_list);
+								if(item.voting_list[week]<2){
+									delete item.voting_list[week];
+								}else{
+									item.voting_list[week]--;
+								}
+								console.log('checking item voting list (vote: 0)', item.voting_list);
 
-						}
-
-						item.markModified('voting_list');
-						item.save(function(err){
-							var returnedUser = {
-									_id: user._id,
-									name: user.name,
-									votes: user.votes,
-									adminLvl: user.adminLvl
 							}
-							res.json({user: returnedUser, item: item})
+							item.markModified('voting_list');
+							item.save(function(err){
+								var returnedUser = {
+										_id: user._id,
+										name: user.name,
+										votes: user.votes,
+										adminLvl: user.adminLvl
+								}
+								res.json(returnedUser)
+							});
 						});
-					})
+					}else if(user.votes[itemId] == req.params.vote){
+						res.json({errors: ['Already voted...']});
+					}else{
+						user.votes[itemId] = -user.votes[itemId];
+						user.markModified('votes');
+						user.save(function(err){
+							if(err){
+								res.json({errors: ['cannot save vote...']});
+							}else{
+								var week = lastSunday();
+								console.log('item voting list.... ', item);
+								if(!(item.voting_list.hasOwnProperty(week))){
+									item.voting_list[week] = 0;
+									console.log('item voting list....2 ', item);
+
+								}
+
+								if(req.params.vote>0){
+									item.voting_list[week]++;
+								}else{
+									if(item.voting_list[week]<2){
+										delete item.voting_list[week];
+									}else{
+										item.voting_list[week]--;
+									}
+								}
+								item.markModified('voting_list');
+								item.save(function(err){
+									var returnedUser = {
+											_id: user._id,
+											name: user.name,
+											votes: user.votes,
+											adminLvl: user.adminLvl
+									}
+									res.json(returnedUser)
+								});
+							}
+						})
+					}
 				})
 			}
 		})
@@ -143,6 +186,27 @@ function ItemsController() {
 				res.json({ item: item });
 			});
 		});
+	}
+	this.walmart = function(req, res){
+		console.log('walmart');
+		console.log(req.params.upcId);
+      var upc = req.params.upcId;
+      walmart.getItemByUPC(upc).then(function(item) {
+          // console.log(item.product.productName);
+          res.json(item.product);
+        });
+	}
+	this.sams = function(req, res){
+		console.log('samssssssss');
+      var itemId = req.params.itemId;
+			console.log(itemId);
+			sams.search(itemId, function(result) {
+			  console.log(result);
+				res.json(result);
+			}, function(err) {
+			  console.log(err);
+				res.json(err);
+			});
 	}
 
 };
